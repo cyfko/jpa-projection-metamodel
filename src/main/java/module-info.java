139ce68d15361d2923a2c204b602b5f31b9c2819
Frank KOSSI <frank.kossi@sprint-pay.com>
2026-04-07 10:02:43 +0100

    remove transitive requirement on java.compiler

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,9 +1,10 @@
 module io.github.cyfko.jpametamodel {
+    requires java.compiler;
+    
     uses io.github.cyfko.jpametamodel.providers.PersistenceRegistryProvider;
     uses io.github.cyfko.jpametamodel.providers.ProjectionRegistryProvider;
 
     exports io.github.cyfko.jpametamodel.api;
     exports io.github.cyfko.jpametamodel.providers;
     exports io.github.cyfko.jpametamodel;
-    requires transitive java.compiler;
 }
